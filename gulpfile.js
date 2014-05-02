ca844093837d7a430d962b032680f8db425c2618
austingreco <austingreco@gmail.com>
2014-05-01 22:09:10 -0500

    Use generator gulpfile
    Use https://github.com/yeoman/generator-gulp-webapp gulpfile
    Renamed src/ back to app/ because of the new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,43 +2,121 @@
 
 var gulp = require('gulp');
 
-// Load plugins
+// load plugins
 var $ = require('gulp-load-plugins')();
 
-// Scripts
-gulp.task('scripts', function() {
-	return gulp.src('src/scripts/**/*.js')
-		.pipe($.jshint('.jshintrc'))
-		.pipe($.jshint.reporter('default'))
+gulp.task('styles', function () {
+	return gulp.src('app/styles/main.css')
+		.pipe($.autoprefixer('last 1 version'))
+		.pipe(gulp.dest('.tmp/styles'))
 		.pipe($.size());
 });
 
-// Default task
-gulp.task('default', function() {
-	gulp.start('watch');
+gulp.task('scripts', function () {
+	return gulp.src('app/scripts/**/*.js')
+		.pipe($.jshint())
+		.pipe($.jshint.reporter(require('jshint-stylish')))
+		.pipe($.size());
 });
 
-// Connect
-gulp.task('connect', $.connect.server({
-	root: ['src'],
-	port: 9000,
-	livereload: true
-}));
+gulp.task('html', ['styles', 'scripts'], function () {
+	var jsFilter = $.filter('**/*.js');
+	var cssFilter = $.filter('**/*.css');
 
-// Watch
-gulp.task('watch', ['connect'], function() {
-	gulp.start('scripts');
+	return gulp.src('app/*.html')
+		.pipe($.useref.assets({searchPath: '{.tmp,app}'}))
+		.pipe(jsFilter)
+		.pipe($.uglify())
+		.pipe(jsFilter.restore())
+		.pipe(cssFilter)
+		.pipe($.csso())
+		.pipe(cssFilter.restore())
+		.pipe($.useref.restore())
+		.pipe($.useref())
+		.pipe(gulp.dest('dist'))
+		.pipe($.size());
+});
 
-	// Watch for changes in `src` folder
+gulp.task('images', function () {
+	return gulp.src('app/images/**/*')
+		.pipe($.cache($.imagemin({
+			optimizationLevel: 3,
+			progressive: true,
+			interlaced: true
+		})))
+		.pipe(gulp.dest('dist/images'))
+		.pipe($.size());
+});
+
+gulp.task('fonts', function () {
+	return $.bowerFiles()
+		.pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+		.pipe($.flatten())
+		.pipe(gulp.dest('dist/fonts'))
+		.pipe($.size());
+});
+
+gulp.task('extras', function () {
+	return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+	return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
+});
+
+gulp.task('build', ['html', 'images', 'fonts', 'extras']);
+
+gulp.task('default', ['clean'], function () {
+	gulp.start('build');
+});
+
+gulp.task('connect', function () {
+	var connect = require('connect');
+	var app = connect()
+		.use(require('connect-livereload')({ port: 35729 }))
+		.use(connect.static('app'))
+		.use(connect.static('.tmp'))
+		.use(connect.directory('app'));
+
+	require('http').createServer(app)
+		.listen(9000)
+		.on('listening', function () {
+			console.log('Started connect web server on http://localhost:9000');
+		});
+});
+
+gulp.task('serve', ['connect'], function () {
+	require('opn')('http://localhost:9000');
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+	var wiredep = require('wiredep').stream;
+
+	gulp.src('app/*.html')
+		.pipe(wiredep({
+			directory: 'app/bower_components'
+		}))
+		.pipe(gulp.dest('app'));
+});
+
+gulp.task('watch', ['connect', 'serve'], function () {
+	var server = $.livereload();
+
+	// watch for changes
+
 	gulp.watch([
-		'src/*.html',
-		'src/scripts/**/*.js',
-		'src/styles/**/*.css'
-	], function(event) {
-		return gulp.src(event.path)
-			.pipe($.connect.reload());
+		'app/*.html',
+		'.tmp/styles/**/*.css',
+		'app/scripts/**/*.js',
+		'app/images/**/*'
+	]).on('change', function (file) {
+		server.changed(file.path);
 	});
 
-	// Watch .js files
-	gulp.watch('src/scripts/**/*.js', ['scripts']);
+	gulp.watch('app/styles/**/*.css', ['styles']);
+	gulp.watch('app/scripts/**/*.js', ['scripts']);
+	gulp.watch('app/images/**/*', ['images']);
+	gulp.watch('bower.json', ['wiredep']);
 });
